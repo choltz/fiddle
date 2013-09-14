9b769e28baa53f7df8aeb66debf80842d3482046
Chris Holtz <choltz@gmail.com>
2013-09-13 22:09:11 -0400

    Added gmaps example 2 - map with css floats.

---
+++ gmaps-getting-started/example2/gmaps.js
@@ -0,0 +1,9 @@
+google.maps.event.addDomListener(window, 'load', function() {
+  var canvas = document.getElementById("map-canvas");
+  var mapOptions = {
+    center: new google.maps.LatLng(40.714352, -74.005973),
+    zoom:   7
+  };
+
+  var map = new google.maps.Map(canvas, mapOptions);
+});
