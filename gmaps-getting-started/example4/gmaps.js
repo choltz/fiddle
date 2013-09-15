3b3bddf81bde118d95b746e5a648b97a550e0506
Chris Holtz <choltz@gmail.com>
2013-09-14 22:01:59 -0400

    Added more gmaps blog post examples

---
+++ gmaps-getting-started/example4/gmaps.js
@@ -0,0 +1,14 @@
+google.maps.event.addDomListener(window, 'load', function() {
+  var canvas = document.getElementById("map-canvas");
+  var mapOptions = {
+    center: new google.maps.LatLng(40.714352, -74.005973),
+    disableDefaultUI: true,
+    zoom:   7
+  };
+
+  var map = new google.maps.Map(canvas, mapOptions);
+
+  $("input[type=radio]").click(function(e) {
+    map.setMapTypeId(e.target.value);
+  });
+});
